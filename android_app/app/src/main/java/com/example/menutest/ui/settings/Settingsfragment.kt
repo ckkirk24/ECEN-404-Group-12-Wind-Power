41b3b373b6dbf3aa8c5cfc130d53bbaffba86288
ckkirk24 <55562784+ckkirk24@users.noreply.github.com>
2023-04-28 02:21:50 -0500

    settings page progress

--- android_app/app/src/main/java/com/example/menutest/ui/settings/Settingsfragment.kt
+++ android_app/app/src/main/java/com/example/menutest/ui/settings/Settingsfragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -23,9 +24,11 @@ class Settingsfragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-
-
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
+        val editText = root.findViewById<EditText>(R.id.edit_text)
+        val desiredChargeLevel = editText.text.toString().toFloatOrNull()
+        // desiredChargeLevel will be null if the user enters an invalid input (e.g., non-numeric value)
+
 
 
         return root
