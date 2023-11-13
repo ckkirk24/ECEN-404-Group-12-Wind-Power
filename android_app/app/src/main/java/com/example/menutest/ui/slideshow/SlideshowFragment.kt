b36591196eca60f8d18f8428db36de8dfaf6f37a
ckkirk24 <55562784+ckkirk24@users.noreply.github.com>
2023-11-12 18:21:28 -0600

    Plotting page fixes

--- android_app/app/src/main/java/com/example/menutest/ui/slideshow/SlideshowFragment.kt
+++ android_app/app/src/main/java/com/example/menutest/ui/slideshow/SlideshowFragment.kt
@@ -20,73 +20,143 @@ import java.text.ParsePosition
 import java.util.*
 import kotlin.math.roundToInt
 
+//class SlideshowFragment : Fragment() {
+//    private var _binding: FragmentSlideshowBinding? = null
+//    private val binding get() = _binding!!
+//    private var callcount = 0
+//    private lateinit var homeViewModel: HomeViewModel
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        // using homeviewmodel to get the live data (battery charge level)
+//        homeViewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
+//
+//        val slideshowViewModel =
+//            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+//
+////        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+//        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
+//
+////        //get the real time battery charge level and initiate replot
+////        homeViewModel.getData1().observe(viewLifecycleOwner) { newData ->
+////
+////            Log.d("SlideshowFragment", "New battery charge level: $newData")
+////
+////        }
+//        //get the real time battery charge level and initiate replot
+//        homeViewModel.getData1().observeForever { newData ->
+//
+//            Log.d("SlideshowFragment", "New battery charge level: $newData")
+//            doPlot(newData)
+//        }
+//
+////        doPlot()
+//
+//
+//        return root
+//
+//
+//    }
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+//
+//    fun doPlot(newChargeLevel: String) {
+//        //val plot = view.findViewById<XYPlot>(R.id.plotCharge)
+//        //callcount++
+//        val plot = binding.plotCharge
+//        //binding.textTest.text = String.format("%5d", callcount)
+////        val chargedArray = arrayOf<Number>(0, 15, 30, 45, 60, 50, 100)
+//
+//        val timeArray = arrayOf<Number>(0, 4, 8, 12, 16, 20, 24)
+//        val chargedArray = timeArray.map { newChargeLevel.toFloatOrNull() ?: 0.0 }.toTypedArray()
+//        val chargedSeries : XYSeries = SimpleXYSeries(
+//            timeArray.asList(), //X vals
+//            chargedArray.asList(), //Y vals
+//
+//            "Charge (%) vs Time (hours)"
+//        )
+//
+//        val chargedFormat = LineAndPointFormatter(Color.RED, Color.GREEN, null, null)
+////        try {
+////            plot.addSeries(chargedSeries, chargedFormat)
+////        }
+////        catch (e: java.lang.NullPointerException){
+////            return
+////        }
+//        plot.clear()  // Clear the existing series
+//        plot.addSeries(chargedSeries, chargedFormat)
+//        PanZoom.attach(plot) //add zooming and panning capabilities
+//        plot.redraw()
+//
+//    }
+//}
+
 class SlideshowFragment : Fragment() {
     private var _binding: FragmentSlideshowBinding? = null
     private val binding get() = _binding!!
-    private var callcount = 0
+
     private lateinit var homeViewModel: HomeViewModel
+    private var timeCounter = 0
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // using homeviewmodel to get the live data (battery charge level)
         homeViewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
 
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
-
-//        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        //get the real time battery charge level and initiate replot
-//        homeViewModel.getData1().observe(viewLifecycleOwner) { newData ->
-//
+        homeViewModel.getData1().observe(viewLifecycleOwner) { newData ->
 //            Log.d("SlideshowFragment", "New battery charge level: $newData")
-//
-//        }
-        //get the real time battery charge level and initiate replot
-        homeViewModel.getData1().observeForever { newData ->
-
-            Log.d("SlideshowFragment", "New battery charge level: $newData")
-
+            timeCounter++
+            val timeArray = generateTimeArray(timeCounter)
+            val chargedArray = generateChargedArray(newData, timeCounter)
+            doPlot(timeArray, chargedArray)
         }
 
-        doPlot()
-
-
         return root
-
-
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
-    fun doPlot() {
-        //val plot = view.findViewById<XYPlot>(R.id.plotCharge)
-        //callcount++
+    private fun generateTimeArray(size: Int): Array<Number> {
+        return Array(size) { it + 1 } // Create an array counting up from 1 to size
+    }
+
+    private fun generateChargedArray(newChargeLevel: String, size: Int): Array<Number> {
+        val cleanedText = newChargeLevel.replace("%", "").trim()
+        Log.d("SlideshowFragment", "trim: $cleanedText")
+        val chargeLevel = cleanedText.toFloatOrNull() ?: 0.0
+//        Log.d("SlideshowFragment", "New battery charge level: $chargeLevel")
+        return Array(size) { chargeLevel }
+    }
+
+    private fun doPlot(timeArray: Array<Number>, chargedArray: Array<Number>) {
         val plot = binding.plotCharge
-        //binding.textTest.text = String.format("%5d", callcount)
-        val chargedArray = arrayOf<Number>(0, 15, 30, 45, 60, 50, 100)
-        val timeArray = arrayOf<Number>(0, 4, 8, 12, 16, 20, 24)
-        val chargedSeries : XYSeries = SimpleXYSeries(
-            timeArray.asList(), //X vals
-            chargedArray.asList(), //Y vals
 
-            "Charge (%) vs Time (hours)"
+        val chargedSeries: XYSeries = SimpleXYSeries(
+            timeArray.asList(),
+            chargedArray.asList(),
+            "Charge (%) vs Time (seconds)"
         )
 
         val chargedFormat = LineAndPointFormatter(Color.RED, Color.GREEN, null, null)
-        try {
-            plot.addSeries(chargedSeries, chargedFormat)
-        }
-        catch (e: java.lang.NullPointerException){
-            return
-        }
+//        chargedFormat.setInterpolationParams(
+//            CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
+//        )
 
-        PanZoom.attach(plot) //add zooming and panning capabilities
+        plot.clear()
+        plot.addSeries(chargedSeries, chargedFormat)
+        plot.setRangeBoundaries(0, 100, BoundaryMode.FIXED) // Adjust the Y-axis range if needed
+        plot.redraw()
     }
 }
+
+
+
+
